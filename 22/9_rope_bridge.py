32975403737999f6745af36ac360e8c3f8451376
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2022-12-14 12:10:50 +0100

    Generalize solution for 9 to fit 9a and 9b

--- 22/9_rope_bridge.py
+++ 22/9_rope_bridge.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
+import math
 
 
 class Dir(Enum):
@@ -20,48 +21,52 @@ def read(filename):
     return movements
 
 
-def m(pos, dir, n=1):
+def move(pos, dir, n=1):
     return (pos[0] + dir.value[0] * n, pos[1] + dir.value[1] * n)
 
 
 def touches(p1, p2):
     return (
         p2 == p1
-        or p2 == m(m(p1, Dir.U), Dir.L)
-        or p2 == m(p1, Dir.U)
-        or p2 == m(m(p1, Dir.U), Dir.R)
-        or p2 == m(p1, Dir.L)
-        or p2 == m(p1, Dir.R)
-        or p2 == m(m(p1, Dir.D), Dir.L)
-        or p2 == m(p1, Dir.D)
-        or p2 == m(m(p1, Dir.D), Dir.R)
+        or p2 == move(move(p1, Dir.U), Dir.L)
+        or p2 == move(p1, Dir.U)
+        or p2 == move(move(p1, Dir.U), Dir.R)
+        or p2 == move(p1, Dir.L)
+        or p2 == move(p1, Dir.R)
+        or p2 == move(move(p1, Dir.D), Dir.L)
+        or p2 == move(p1, Dir.D)
+        or p2 == move(move(p1, Dir.D), Dir.R)
     )
 
 
-def move(pos_h, pos_t, dir):
-    pos_h_new = m(pos_h, dir)
-    if touches(pos_h_new, pos_t):
-        return pos_h_new, pos_t
-    if pos_h[0] == pos_t[0] or pos_h[1] == pos_t[1]:  # H and T on same row or column
-        pos_t_new = m(pos_t, dir)
+def normalize(vec):
+    norm = math.sqrt(vec[0] ** 2 + vec[1] ** 2)
+    return (vec[0] / norm, vec[1] / norm)
+
+
+def follow(pos_h, pos_t):
+    if touches(pos_h, pos_t):
+        return pos_t
+    dir = normalize((pos_h[0] - pos_t[0], pos_h[1] - pos_t[1]))
+    if dir in [d.value for d in Dir]:  # H and T on same row or column
+        dir = Dir(dir)
+        pos_t_new = move(pos_t, dir)
     else:
-        pos_t_new = m(pos_t, dir)
-        if dir == Dir.U or dir == Dir.D:
-            pos_t_new = m(pos_t_new, Dir.L if pos_h_new[0] < pos_t_new[0] else Dir.R)
-        else:
-            pos_t_new = m(pos_t_new, Dir.U if pos_h_new[1] > pos_t_new[1] else Dir.D)
-    return pos_h_new, pos_t_new
+        dir = (dir[0] / abs(dir[0]), dir[1] / abs(dir[1]))
+        pos_t_new = move(pos_t, Dir((dir[0], 0)))
+        pos_t_new = move(pos_t_new, Dir((0, dir[1])))
+    return pos_t_new
 
 
-def pretty_print(tail_visited):
-    max_x = max(tail_visited, key=lambda p: p[0])[0]
-    min_x = min(tail_visited, key=lambda p: p[0])[0]
-    max_y = max(tail_visited, key=lambda p: p[1])[1]
-    min_y = min(tail_visited, key=lambda p: p[1])[1]
+def pretty_print(positions):
+    max_x = max(positions, key=lambda p: p[0])[0]
+    min_x = min(positions, key=lambda p: p[0])[0]
+    max_y = max(positions, key=lambda p: p[1])[1]
+    min_y = min(positions, key=lambda p: p[1])[1]
     for j in range(max_y, min_y - 1, -1):
         l = ""
         for i in range(min_x, max_x + 1):
-            if (i, j) in tail_visited:
+            if (i, j) in positions:
                 if i == 0 and j == 0:
                     l += "s"
                 else:
@@ -71,14 +76,27 @@ def pretty_print(tail_visited):
         print(l)
 
 
-if __name__ == "__main__":
-    movements = read("input/9.txt")
-    pos_h = (0, 0)
-    pos_t = (0, 0)
-    tail_visited = {pos_t}
+def visit(movements, n):
+    positions = [(0, 0) for _ in range(n)]
+    tail_visited = {(0, 0)}
     for dir, steps in movements:
         for _ in range(steps):
-            pos_h, pos_t = move(pos_h, pos_t, dir)
-            tail_visited.add(pos_t)
+            positions[0] = move(positions[0], dir)
+            for i in range(len(positions) - 1):
+                positions[i + 1] = follow(positions[i], positions[i + 1])
+            tail_visited.add(positions[-1])
+    return tail_visited
+
+
+if __name__ == "__main__":
+    movements = read("input/9.txt")
+
+    n = 2
+    tail_visited = visit(movements, n)
+    pretty_print(tail_visited)
+    print(len(tail_visited))
+
+    n = 10
+    tail_visited = visit(movements, n)
     pretty_print(tail_visited)
     print(len(tail_visited))
