9da02fbabd1e5e8183e5ef2234598c0879aff643
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2022-12-19 15:28:41 +0100

    Add solution for 13b

--- 22/13_distress_signal.py
+++ 22/13_distress_signal.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+import functools
 
 
 def read(filename):
@@ -12,7 +13,7 @@ def read(filename):
 
 def correct(p, q):
     if type(p) is int and type(q) is int:
-        return -1 if p == q else p < q
+        return 0 if p == q else -1 if p < q else 1
     elif type(p) is int:
         p = [p]
     elif type(q) is int:
@@ -26,22 +27,20 @@ def correctly_ordered_pair(p, q):
     length = max(len(p), len(q))
     for i in range(length):
         if i == len(p):
-            return True
+            return -1
         if i == len(q):
-            return False
+            return 1
         res = correct(p[i], q[i])
-        if not res:
-            return False
-        elif res == 1:
-            return True
-    return -1
+        if res != 0:
+            return res
+    return 0
 
 
 def correctly_ordered_pairs(pairs):
     correctly_ordered_indices = []
     for i, pair in enumerate(pairs, start=1):
         res = correctly_ordered_pair(pair[0], pair[1])
-        if res and res != -1:
+        if res == -1:
             correctly_ordered_indices.append(i)
     return correctly_ordered_indices
 
@@ -50,3 +49,12 @@ if __name__ == "__main__":
     pairs = read("input/13.txt")
     correctly_ordered_indices = correctly_ordered_pairs(pairs)
     print(sum(correctly_ordered_indices))
+
+    signals = [s for p in pairs for s in p]
+    divider_packet_1 = [[2]]
+    divider_packet_2 = [[6]]
+    signals.append(divider_packet_1)
+    signals.append(divider_packet_2)
+
+    signals.sort(key=functools.cmp_to_key(correctly_ordered_pair))
+    print((signals.index(divider_packet_1) + 1) * (signals.index(divider_packet_2) + 1))
