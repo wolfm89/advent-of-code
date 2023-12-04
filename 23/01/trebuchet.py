1e53f55532d22f054666981fbaa5b05abd091e65
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-12-04 18:43:41 +0100

    Readd solution for part 1 (day 1)

--- 23/01/trebuchet.py
+++ 23/01/trebuchet.py
@@ -16,7 +16,13 @@ def str_to_int(string):
     except ValueError:
         return numbers.index(string)
 
-def get_calibration_value(line):
+def get_calibration_value_part1(line):
+    digits = re.findall(r"\d", line)
+    first_digit = str_to_int(digits[0])
+    last_digit = str_to_int(digits[-1])
+    return first_digit * 10 + last_digit
+
+def get_calibration_value_part2(line):
     """
     Get the calibration value of a line of text.
     The calibration value is the sum of the first and last digits in the line.
@@ -30,8 +36,12 @@ def get_calibration_value(line):
 
 if __name__ == "__main__":
     with open("input/data.txt", "r") as reader:
-        sum = 0
+        sum_part1 = 0
+        sum_part2 = 0
         for line in reader:
-            calibration_value = get_calibration_value(line.rstrip())
-            sum += calibration_value
-        print(sum)
+            calibration_value_part1 = get_calibration_value_part1(line.rstrip())
+            calibration_value_part2 = get_calibration_value_part2(line.rstrip())
+            sum_part1 += calibration_value_part1
+            sum_part2 += calibration_value_part2
+        print(sum_part1)
+        print(sum_part2)
